129c43bc14a03a913075d997fa602ea7af5d897b
CodeTristan <5aerenmertozen@gmail.com>
2022-10-21 19:19:53 +0300

    1

---
+++ GreenyGameJam/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+        transform.position += new Vector3(horizontal, vertical,0);
+
+
+    }
+}
