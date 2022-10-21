8aff7cdfb6bb418aac63e2bbaf9c6133b892dfcb
bcgkky <bcgkky1@gmail.com>
2022-10-21 21:40:24 +0300

    1

--- GreenyGameJam/Assets/Inventory.cs
+++ GreenyGameJam/Assets/Inventory.cs
@@ -6,36 +6,36 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    List<Item> inventory = new List<Item>();
+    //List<Item> inventory = new List<Item>();
     
     
     
 
     
-    private bool onObject = false;
-    private string tempItem = "";
-    private GameObject tempObject;
-    private void OnTriggerEnter2D(Collider2D collider2D)
-    {
-        if(collider2D.tag == "Item")
-        {
-            onObject = true;
-            tempItem = collider2D.gameObject.name;
-            tempObject = collider2D.gameObject;
-        }
-    }
-    private void OnTriggerExit2D(Collider2D collider2D)
-    {
-        if (collider2D.tag == "Item")
-            onObject = false;
-    }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) && onObject)
-        {
-            inventory.Add(tempItem);
-            Destroy(tempObject);
-            Debug.Log("envanter : " + inventory.Count);
-        }
-    }
+    //private bool onObject = false;
+    //private string tempItem = "";
+    //private GameObject tempObject;
+    //private void OnTriggerEnter2D(Collider2D collider2D)
+    //{
+    //    if(collider2D.tag == "Item")
+    //    {
+    //        onObject = true;
+    //        tempItem = collider2D.gameObject.name;
+    //        tempObject = collider2D.gameObject;
+    //    }
+    //}
+    //private void OnTriggerExit2D(Collider2D collider2D)
+    //{
+    //    if (collider2D.tag == "Item")
+    //        onObject = false;
+    //}
+    //void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.Space) && onObject)
+    //    {
+    //        //inventory.Add(tempItem);
+    //        Destroy(tempObject);
+    //        Debug.Log("envanter : " + inventory.Count);
+    //    }
+    //}
 }
