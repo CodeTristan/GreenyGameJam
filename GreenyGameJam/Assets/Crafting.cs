497a6c9ef7f672108f3ee5685a7c9ebacf71333f
bcgkky <bcgkky1@gmail.com>
2022-10-22 15:25:19 +0300

    11

--- GreenyGameJam/Assets/Crafting.cs
+++ GreenyGameJam/Assets/Crafting.cs
@@ -81,4 +81,62 @@ public class Crafting : MonoBehaviour
             FindObjectOfType<Inventory>().RefleshInventoryUI();
         }
     }
+    public void Craft2()
+    {
+        if (itemName == otherCrafting.itemName)
+        {
+            item.ItemName = itemName;
+            item.sprite = sprite;
+            FindObjectOfType<Inventory>().AddItem(item);
+        }
+        if ((itemName == "Toprak" && otherCrafting.itemName == "Su") || (otherCrafting.itemName == "Toprak" && itemName == "Su"))
+        {
+            item.ItemName = "Çamur";
+            item.sprite = greenSprite; //todo : camurSprite
+            FindObjectOfType<Inventory>().AddItem(item);
+            crafted[0] = true;
+            flasks[0].SetActive(true);
+        }
+        else if ((itemName == "Na" && otherCrafting.itemName == "Cl") || (otherCrafting.itemName == "Na" && itemName == "Cl"))
+        {
+            item.ItemName = "Tuz";
+            item.sprite = purpleSprite; //to do : tuzSprite
+            FindObjectOfType<Inventory>().AddItem(item);
+            crafted[1] = true;
+            flasks[1].SetActive(true);
+        }
+        else if ((itemName == "Su" && otherCrafting.itemName == "Tuz") || (otherCrafting.itemName == "Su" && itemName == "Tuz"))
+        {
+            item.ItemName = "TuzluSu";
+            item.sprite = orangeSprite; //to do : tuzlusuSprite
+            FindObjectOfType<Inventory>().AddItem(item);
+            crafted[2] = true;
+            flasks[2].SetActive(true);
+        }
+        else if ((itemName == "Su" && otherCrafting.itemName == "AsedikAsit") || (otherCrafting.itemName == "Su" && itemName == "AsedikAsit"))
+        {
+            item.ItemName = "Sirke";
+            item.sprite = orangeSprite; //to do : sirkeSprite
+            FindObjectOfType<Inventory>().AddItem(item);
+            crafted[3] = true; //?
+            flasks[3].SetActive(true); //?
+        }
+
+        itemName = "";
+        sprite = null;
+        otherCrafting.itemName = "";
+        otherCrafting.sprite = null;
+        gameObject.GetComponentInChildren<SpriteRenderer>().sprite = sprite;
+        otherCrafting.gameObject.GetComponentInChildren<SpriteRenderer>().sprite = sprite;
+
+        if (crafted[0] && crafted[1] && crafted[2] && crafted[3])
+        {
+            FindObjectOfType<Inventory>().inventory.Clear();
+            FindObjectOfType<Inventory>().RefleshInventoryUI();
+            item.ItemName = "FinalFlask2";
+            item.sprite = finalFlashSprite; // ?
+            FindObjectOfType<Inventory>().AddItem(item);
+            FindObjectOfType<Inventory>().RefleshInventoryUI();
+        }
+    }
 }
