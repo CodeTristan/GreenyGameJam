91099b87b9aaae42eed9c4de2a0007051460b1bd
CodeTristan <5aerenmertozen@gmail.com>
2022-10-23 09:41:21 +0300

    123

--- GreenyGameJam/Assets/menuManager.cs
+++ GreenyGameJam/Assets/menuManager.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class menuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject main;
+    public GameObject options;
+    public GameObject credits;
+
+
+    public void Exit()
     {
-        
+        Application.Quit();
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void CloseAllScenes()
+    {
+        main.SetActive(false);
+        options.SetActive(false);
+        credits.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenMain()
     {
-        
+        CloseAllScenes();
+        main.SetActive(true);
+    }
+    public void OpenOptions()
+    {
+        CloseAllScenes();
+        options.SetActive(true);
+    }
+    public void OpenCredits()
+    {
+        CloseAllScenes();
+        credits.SetActive(true);
     }
 }
